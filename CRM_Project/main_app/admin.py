50959d0568781e6563180984e28eab1dec51fbf5
jack <whitefrostcat@gmail.com>
2022-11-19 18:36:59 +0500

    Init. Make base model and admin.

---
+++ CRM_Project/main_app/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Client, Order, Brand, Unit, Model
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname', 'phone_number', 'address', ]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['client', 'unit', ]
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['brand', ]
+
+
+@admin.register(Model)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['model', ]
+
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ['brand', 'model', ]
+
+
---
+++ CRM_Project/main_app/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.core.validators import RegexValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Client(models.Model):
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    name = models.CharField(max_length=40, verbose_name='Имя')
+    surname = models.CharField(max_length=40, verbose_name='Фамилия')
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True, verbose_name='Номер телефона')  # Validators should be a list
+    address = models.CharField(max_length=40, verbose_name='Адрес')
+
+    def __str__(self):
+        return f'{self.name} {self.surname} {self.phone_number}'
+
+
+class Brand(models.Model):
+    brand = models.CharField(max_length=40, default='No brand', verbose_name='Бренд', unique=True)
+
+    def __str__(self):
+        return f'{self.brand}'
+
+
+class Model(models.Model):
+    model = models.CharField(max_length=40,verbose_name='Модель', unique=True)
+
+    def __str__(self):
+        return f'{self.model}'
+
+
+class Unit(models.Model):
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='unit', null=True, verbose_name='Бренд')
+    model = models.ForeignKey(Model, on_delete=models.PROTECT, related_name='unit', null=True, verbose_name='Модель')
+    serial_number = models.CharField(max_length=42, default='No serial', verbose_name='Серийный номер')
+
+    def __str__(self):
+        return f'{self.brand} {self.model}'
+
+
+class Order(models.Model):
+    defect = models.TextField(max_length=120, null=True, verbose_name='Описание дефекта')
+    diagnostic_result = models.TextField(max_length=400, null=True, verbose_name='Результат диагностики')
+    works = models.TextField(max_length=500, null=True, verbose_name='Выполненные работы')
+    finally_price = models.IntegerField(validators=[MinValueValidator(0)], null=True, blank=True, default=0, verbose_name='Сумма ремонта')
+
+    client = models.ForeignKey(Client, on_delete=models.PROTECT, related_name='order', verbose_name='Клиент')
+    unit = models.ForeignKey(Unit, on_delete=models.PROTECT, related_name='order', null=True, verbose_name='Техника')
+
+    def __str__(self):
+        return f'{self.pk}'
