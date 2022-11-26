3700965e1fb9dffa9a06733a6fd479af6ab1c624
jack <whitefrostcat@gmail.com>
2022-11-27 00:40:07 +0500

    add base template and index page

--- CRM_Project/main_app/views.py
+++ CRM_Project/main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from .models import Order
 
-# Create your views here.
+
+class Index(ListView):
+    template_name = 'main_app/index.html'
+    model = Order
+    context_object_name = 'orders'
+
