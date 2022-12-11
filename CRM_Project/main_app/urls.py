bdf022db2a33453ee20cbcc48b637ea994f52620
jack <whitefrostcat@gmail.com>
2022-12-11 21:41:42 +0500

    Add bootstrap style for some elements

--- CRM_Project/main_app/urls.py
+++ CRM_Project/main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Index.as_view()),
+    path('', views.Index.as_view(),name='home'),
     path('new_order', views.NewOrder.as_view(), name='add-new-order'),
     path('order/<int:val>', views.GetOrder.as_view(), name='get-order'),
 
