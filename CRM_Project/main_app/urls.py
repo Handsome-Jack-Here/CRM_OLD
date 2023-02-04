9dc6f3a615fabfaf6dd9cc5300346fed7f111fd7
Jack <jack@jack.com>
2023-02-04 23:54:35 +0500

    Control commit

--- CRM_Project/main_app/urls.py
+++ CRM_Project/main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.Index.as_view(),name='home'),
     path('new_order', views.NewOrder.as_view(), name='add-new-order'),
-    path('order/<int:val>', views.GetOrder.as_view(), name='get-order'),
+    path('order/<int:val>/', views.GetOrder.as_view(), name='get-order'),
     path('order/client-edit/<int:val>', views.EditClient.as_view(), name='edit-client'),
 
 
